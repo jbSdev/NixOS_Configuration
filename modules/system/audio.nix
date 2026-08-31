0ce3dd578a9573bb8ca24221852a9f51af74699b
jbSdev <antoni.jesien@gmail.com>
2026-08-31 12:33:28 -0500

    set laptop speakers as default

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -11,9 +11,25 @@
             alsa.enable = true;
             alsa.support32Bit = true;
             pulse.enable = true;
+
+            wireplumber.extraConfig = {
+                "51-audio-priority" = {
+                    "monitor.alsa.rules" = [
+                        {
+                            matches = [{
+                                    "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI1__sink";
+                            }];
+                            actions = {
+                                "update-props" = { "priority.session" = 200; };
+                            };
+                        }
+                    ];
+                };
+            };
         };
 
         users.users.jb.extraGroups = [ "audio" ];
+
 
     };
 
